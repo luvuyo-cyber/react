0418d7c8940b2e3d2b42800c1f8e3f4256431648
luvuyo_mazibuko <fisokuhlelavuyo97@gmail.com>
2024-06-12 01:40:18 +0200

    ch6

---
+++ ch_6/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import UserForm from './UserForm';
+// import Products from './Products';
+// import JumboTronComponent from './JumboTronComponent';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <Products /> */}
+        {/* <JumboTronComponent />  */}
+        <UserForm />
+      </div>
+    );
+  }
+}
+
+export default App;
