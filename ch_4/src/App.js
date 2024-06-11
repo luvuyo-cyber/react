e4aada9ef30ec42e00bca5b20334e1be679f4108
luvuyo_mazibuko <fisokuhlelavuyo97@gmail.com>
2024-06-11 13:26:07 +0200

    ch4

---
+++ ch_4/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Products from './Products';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Products />
+      </div>
+    );
+  }
+}
+
+export default App;
