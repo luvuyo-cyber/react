d394bc8e7e254eee72d2fca4b1ac1f8b4a7a536a
luvuyo_mazibuko <fisokuhlelavuyo97@gmail.com>
2024-06-12 03:33:02 +0200

    ch7

---
+++ ch_7/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import GitHub from './GitHub';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <GitHub />
+      </div>
+    );
+  }
+}
+
+export default App;
